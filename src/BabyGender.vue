ca8a7306c0351578b4efb6bb23187589bf26f3e0
camwood23 <camwood23@gmail.com>
2020-05-02 10:41:43 -0400

    Gender reveal page

--- src/BabyGender.vue
+++ src/BabyGender.vue
@@ -1,6 +1,15 @@
 <template>
-  <div id="app">
-    <h1>Hello Baby</h1>
+  <div id="app" class="container">
+    <h1 class="gender">
+      It's
+      <br />
+      <span class="underline">A B</span>oy!
+    </h1>
+
+    <h2 class="due_date">
+      Due Date August
+      <br />31 2020
+    </h2>
   </div>
 </template>
 
@@ -11,5 +20,42 @@ export default {
 };
 </script>
 
-<style>
+<style scoped=true>
+.container {
+  background-image: url(../public/img/baby_backdrop.jpg) !important;
+}
+
+.underline {
+  border-bottom: 0.4rem solid #000;
+  padding-bottom: 0.3rem;
+}
+
+.gender {
+  font-size: 7rem;
+  font-weight: 400;
+  position: fixed;
+  top: 10rem;
+  left: 10rem;
+  font-family: "Bebas Neue", cursive;
+}
+
+.due_date {
+  font-size: 4rem;
+  font-weight: 400;
+  transform: rotate(-90deg);
+
+  position: fixed;
+  bottom: 20rem;
+  left: 0;
+  font-family: "Anonymous Pro", monospace;
+}
+
+@media only screen and (max-width: 37.5em) {
+  .gender {
+    font-size: 10rem;
+  }
+  .due_date {
+    font-size: 5rem;
+  }
+}
 </style>
