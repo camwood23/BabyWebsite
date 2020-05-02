279d5b664b67c63a735110704c947a0590d5d057
camwood23 <camwood23@gmail.com>
2020-05-02 09:13:37 -0400

    Added vue framework

--- public/js/extra.js
+++ public/js/extra.js
@@ -1,6 +1,6 @@
 var countDownDate = new Date(2020, 4, 2, 12).getTime();
 
-var x = setInterval(function () {
+var x = setInterval(function() {
   var now = new Date().getTime();
   var distance = countDownDate - now;
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -21,10 +21,11 @@ var x = setInterval(function () {
 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("timer").innerHTML = "EXPIRED";
+    document.getElementById("timer").innerHTML =
+      "Whoops, you didn't get all the changes. Try re-loading the page and clearing your cache!";
   }
 }, 1000);
 
-$(".menu-btn__clickable").click(function () {
+$(".menu-btn__clickable").click(function() {
   $(".menu-btn").toggleClass("active");
 });
---
+++ src/BabyGender.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>Hello Baby</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloBaby",
+  components: {}
+};
+</script>
+
+<style>
+</style>
